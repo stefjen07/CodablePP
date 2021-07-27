739849997ec4f4709e2cf098bf24c8b3fcd4f93e
Yauheni Stsefankou <stefjen@mail.ru>
2021-07-27 17:19:54 +0300

    Bugfix

--- JSON.cpp
+++ JSON.cpp
@@ -50,34 +50,6 @@ void JSONEncodeContainer::generateContent() {
     }
 }
 
-//Encoding vector as array
-template <typename T>
-void JSONEncodeContainer::encode(vector<T> value, CodingKey key) {
-    //Creating container for array
-    JSONEncodeContainer array(this->containers);
-    //Setting container's key to the value of a given key
-    array.key = key;
-    //Setting encoding type to array
-    array.encodingType = JSONContainerType::array;
-    //Encoding every element of array
-    for (int i = 0; i < value.size(); i++) {
-        //Creating container for element
-        JSONEncodeContainer valueContainer(containers);
-        //Encoding element to container
-        valueContainer.encode(value[i]);
-        //Adding encoded container to array's container
-        containers->push_back(valueContainer);
-        //Adding encoded container to array's children
-        array.childrenIndexes.push_back(containers->size() - 1);
-    }
-    //Generate JSON respresentation of array
-    array.generateContent();
-    //Adding encoded container to containers array
-    containers->push_back(array);
-    //Adding encoded container to children array of current container
-    childrenIndexes.push_back(containers->size() - 1);
-}
-
 //Encoding method implementation for boolean
 void JSONEncodeContainer::encode(bool value, CodingKey key) {
     JSONEncodeContainer result(key, "", containers);
@@ -146,66 +118,6 @@ void JSONEncodeContainer::encode(string value, CodingKey key, bool withQuotes) {
     result.generateContent();
     containers->push_back(result);
     childrenIndexes.push_back(containers->size() - 1);
-}
-
-//Encoding method for classes with Codable protocol 
-template <class T>
-void JSONEncodeContainer::encode(T value, CodingKey key) {
-    //Creating container for result
-    JSONEncodeContainer result(this->containers);
-    //Setting container's key to the value of a given key
-    result.key = key;
-    //Setting encoding type to closure
-    result.encodingType = JSONContainerType::closure;
-    //Checking if provided type is polymorphic
-    if (is_polymorphic<T>::value) {
-        //Converting provided value to Codable
-        Codable* casted = dynamic_cast<Codable*>(&value);
-        //Encoding provided value to result container
-        casted->encode(&result);
-    }
-    //Generating JSON representation of result container content
-    result.generateContent();
-    //Adding result container to containers array
-    containers->push_back(result);
-    //Adding result container to children array of current container
-    childrenIndexes.push_back(containers->size() - 1);
-}
-
-//Encoding method for classes with Codable protocol without key
-template <class T>
-void JSONEncodeContainer::encode(T value) {
-    //Setting encoding type to closure
-    encodingType = JSONContainerType::closure;
-    //Checking if provided type is polymorphic
-    if (is_polymorphic<T>::value) {
-        //Converting provided value to Codable
-        Codable* casted = dynamic_cast<Codable*>(&value);
-        //Encoding provided value to result container
-        casted->encode(this);
-    }
-    //Generating JSON representation of container content
-    generateContent();
-}
-
-//Encoding vector as array without key
-template <typename T>
-void JSONEncodeContainer::encode(vector<T> value) {
-    //Setting encoding type to array
-    encodingType = JSONContainerType::array;
-    //Encoding every element of array
-    for (int i = 0; i < value.size(); i++) {
-        //Creating a container for element
-        JSONEncodeContainer valueContainer(containers);
-        //Encoding element to container
-        valueContainer.encode(value[i]);
-        //Adding element to containers vector
-        containers->push_back(valueContainer);
-        //Adding element to children vector of array container
-        childrenIndexes.push_back(containers->size() - 1);
-    }
-    //Generating JSON representation of container content
-    generateContent();
 }
 
 //Method that debeautifies JSON content (removes spaces and line breaks)
@@ -417,68 +329,6 @@ string JSONDecodeContainer::decode(string type, CodingKey key, bool withQuotes) 
         value.erase(value.begin() + value.length() - 1);
     }
     return value;
-}
-
-//Decoding method for classes with Codable protocol
-template <class T>
-T JSONDecodeContainer::decode(T type, CodingKey key) {
-    //Checking if provided type is polymorphic
-    if (is_polymorphic<T>::value) {
-        //Converting type sample to Codable
-        Codable* casted = dynamic_cast<Codable*>(&type);
-        //Getting container for decoding
-        JSONDecodeContainer* jsonContainer = operator[](key);
-        //If key is empty, then decoding to current container
-        if (key == MAIN_CONTAINER_KEY) {
-            jsonContainer = this;
-        }
-        if (jsonContainer != NULL) {
-            //Converting JSON container to CoderContainer
-            CoderContainer* container = static_cast<CoderContainer*>(jsonContainer);
-            if (container != NULL) {
-                //Decoding value to container
-                casted->decode(container);
-            }
-        }
-    }
-    //Returning modified type sample
-    return type;
-}
-
-//Decoding method for array
-template <typename T>
-vector<T> JSONDecodeContainer::decode(vector<T> type, CodingKey key) {
-    //Creating vector for result
-    vector<T> result;
-    //Getting container for decoding
-    auto array = operator[](key);
-    if (array != NULL) {
-        //Decoding every element of array
-        for (int i = 0; i < array->childrenIndexes.size(); i++) {
-            T item = containers->data()[array->childrenIndexes[i]].decode(T());
-            result.push_back(item);
-        }
-    }
-    return result;
-}
-
-//Decoding method for array without key (array in this container)
-template <typename T>
-vector<T> JSONDecodeContainer::decode(vector<T> type) {
-    //Creating vector for result
-    vector<T> result;
-    //Decoding every element of array
-    for (int i = 0; i < childrenIndexes.size(); i++) {
-        T item = containers->data()[childrenIndexes[i]].decode(T());
-        result.push_back(item);
-    }
-    return result;
-}
-
-//Decoding method for all fields without key
-template<typename T>
-T JSONDecodeContainer::decode(T type) {
-    return decode(type, MAIN_CONTAINER_KEY);
 }
 
 JSONEncodeContainer JSONEncoder::container() {
--- JSON.h
+++ JSON.h
@@ -48,7 +48,31 @@ public:
 
     //Encoding vector as array
     template <typename T>
-    void encode(vector<T> value, CodingKey key);
+    void encode(vector<T> value, CodingKey key) {
+        //Creating container for array
+        JSONEncodeContainer array(this->containers);
+        //Setting container's key to the value of a given key
+        array.key = key;
+        //Setting encoding type to array
+        array.encodingType = JSONContainerType::array;
+        //Encoding every element of array
+        for (int i = 0; i < value.size(); i++) {
+            //Creating container for element
+            JSONEncodeContainer valueContainer(containers);
+            //Encoding element to container
+            valueContainer.encode(value[i]);
+            //Adding encoded container to array's container
+            containers->push_back(valueContainer);
+            //Adding encoded container to array's children
+            array.childrenIndexes.push_back(containers->size() - 1);
+        }
+        //Generate JSON respresentation of array
+        array.generateContent();
+        //Adding encoded container to containers array
+        containers->push_back(array);
+        //Adding encoded container to children array of current container
+        childrenIndexes.push_back(containers->size() - 1);
+    }
 
     //Encoding methods for standard data types 
 
@@ -61,15 +85,63 @@ public:
 
     //Encoding method for classes with Codable protocol 
     template <class T>
-    void encode(T value, CodingKey key);
+    void encode(T value, CodingKey key) {
+        //Creating container for result
+        JSONEncodeContainer result(this->containers);
+        //Setting container's key to the value of a given key
+        result.key = key;
+        //Setting encoding type to closure
+        result.encodingType = JSONContainerType::closure;
+        //Checking if provided type is polymorphic
+        if (is_polymorphic<T>::value) {
+            //Converting provided value to Codable
+            Codable* casted = dynamic_cast<Codable*>(&value);
+            //Encoding provided value to result container
+            casted->encode(&result);
+        }
+        //Generating JSON representation of result container content
+        result.generateContent();
+        //Adding result container to containers array
+        containers->push_back(result);
+        //Adding result container to children array of current container
+        childrenIndexes.push_back(containers->size() - 1);
+    }
 
     //Encoding method for classes with Codable protocol without key
     template <class T>
-    void encode(T value);
+    void encode(T value) {
+        //Setting encoding type to closure
+        encodingType = JSONContainerType::closure;
+        //Checking if provided type is polymorphic
+        if (is_polymorphic<T>::value) {
+            //Converting provided value to Codable
+            Codable* casted = dynamic_cast<Codable*>(&value);
+            //Encoding provided value to result container
+            casted->encode(this);
+        }
+        //Generating JSON representation of container content
+        generateContent();
+    }
 
     //Encoding vector as array without key
     template <typename T>
-    void encode(vector<T> value);
+    void encode(vector<T> value) {
+        //Setting encoding type to array
+        encodingType = JSONContainerType::array;
+        //Encoding every element of array
+        for (int i = 0; i < value.size(); i++) {
+            //Creating a container for element
+            JSONEncodeContainer valueContainer(containers);
+            //Encoding element to container
+            valueContainer.encode(value[i]);
+            //Adding element to containers vector
+            containers->push_back(valueContainer);
+            //Adding element to children vector of array container
+            childrenIndexes.push_back(containers->size() - 1);
+        }
+        //Generating JSON representation of container content
+        generateContent();
+    }
 
     JSONEncodeContainer(vector<JSONEncodeContainer>* containers);
     JSONEncodeContainer(string key, string content, vector<JSONEncodeContainer>* containers);
@@ -98,19 +170,65 @@ public:
     
     //Decoding method for classes with Codable protocol
     template <class T>
-    T decode(T type, CodingKey key);
+    T decode(T type, CodingKey key) {
+        //Checking if provided type is polymorphic
+        if (is_polymorphic<T>::value) {
+            //Converting type sample to Codable
+            Codable* casted = dynamic_cast<Codable*>(&type);
+            //Getting container for decoding
+            JSONDecodeContainer* jsonContainer = operator[](key);
+            //If key is empty, then decoding to current container
+            if (key == MAIN_CONTAINER_KEY) {
+                jsonContainer = this;
+            }
+            if (jsonContainer != NULL) {
+                //Converting JSON container to CoderContainer
+                CoderContainer* container = static_cast<CoderContainer*>(jsonContainer);
+                if (container != NULL) {
+                    //Decoding value to container
+                    casted->decode(container);
+                }
+            }
+        }
+        //Returning modified type sample
+        return type;
+    }
 
     //Decoding method for array
     template <typename T>
-    vector<T> decode(vector<T> type, CodingKey key);
+    vector<T> decode(vector<T> type, CodingKey key) {
+        //Creating vector for result
+        vector<T> result;
+        //Getting container for decoding
+        auto array = operator[](key);
+        if (array != NULL) {
+            //Decoding every element of array
+            for (int i = 0; i < array->childrenIndexes.size(); i++) {
+                T item = containers->data()[array->childrenIndexes[i]].decode(T());
+                result.push_back(item);
+            }
+        }
+        return result;
+    }
 
     //Decoding method for array without key (array in this container)
     template <typename T>
-    vector<T> decode(vector<T> type);
+    vector<T> decode(vector<T> type) {
+        //Creating vector for result
+        vector<T> result;
+        //Decoding every element of array
+        for (int i = 0; i < childrenIndexes.size(); i++) {
+            T item = containers->data()[childrenIndexes[i]].decode(T());
+            result.push_back(item);
+        }
+        return result;
+    }
     
     //Decoding method for all fields without key
     template<typename T>
-    T decode(T type);
+    T decode(T type) {
+        return decode(type, MAIN_CONTAINER_KEY);
+    }
 
     JSONDecodeContainer(string content, vector<JSONDecodeContainer>* containers);
     JSONDecodeContainer(string key, string content, vector<JSONDecodeContainer>* containers);
