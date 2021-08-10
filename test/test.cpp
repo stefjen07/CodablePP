8569cb25c4949cec9da06f3ed71eb7bc6ec9e38a
Yauheni Stsefankou <stefjen@mail.ru>
2021-08-11 02:41:38 +0300

    Bugfix

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,8 @@
 //Example of using Codable protocol and JSON encoder/decoder
 //Description: Encoding phone book class to JSON string, then decoding and printing all the contacts in the book. 
 
+#define _USE_MATH_DEFINES
+
 #include "Codable.hpp"
 #include "JSON.hpp"
 #include <iostream>
